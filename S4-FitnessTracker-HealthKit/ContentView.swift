5a8f3c555612b3056e405b7195f0afa8264c885a
Reno Muijsenberg <r.muijsenberg@student.fontys.nl>
2023-02-17 10:52:31 +0100

    first commit

---
+++ S4-FitnessTracker-HealthKit/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  S4-FitnessTracker-HealthKit
+//
+//  Created by Reno Muijsenberg on 17/02/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ S4-FitnessTracker-HealthKit/S4_FitnessTracker_HealthKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  S4_FitnessTracker_HealthKitApp.swift
+//  S4-FitnessTracker-HealthKit
+//
+//  Created by Reno Muijsenberg on 17/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct S4_FitnessTracker_HealthKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
